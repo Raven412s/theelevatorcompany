348daa0d1a2e687698e87a90da20bbe4f8116dc4
Ashutosh Sharan <being.ashutosh16.20@gmail.com>
2025-01-29 17:13:44 +0530

    improvised in the products page and incorporated the smooth scrolling

--- src/components/HOC/NavFooterWrapper.tsx
+++ src/components/HOC/NavFooterWrapper.tsx
@@ -1,28 +1,8 @@
-"use client"
-import React, { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren } from 'react'
 import Navbar from '../Navbar'
 import Contact from '../custom/Contact'
-import Lenis from '@studio-freight/lenis'
 
 const NavFooterWrapper = ({children}: PropsWithChildren) => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
 
   return (
     <div>
